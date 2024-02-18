d4f1fa95147b7d7d3917fcf60d758fc1405f1fa3
Jau2002 <idamendjaume@gmail.com>
2024-02-17 23:14:44 -0500

    feat: add experience component

---
+++ src/helpers/date-parse.ts
@@ -0,0 +1,22 @@
+import { work } from '@/static/resume-es.json'
+import capitalize from 'just-capitalize'
+
+function normaliceDate(time: string): string {
+	const date: Date = new Date(time)
+
+	const dateYear: number = date.getFullYear()
+
+	const dateAbbreviationMonth: string = capitalize(
+		date.toLocaleString('es-ES', {
+			month: 'short',
+		})
+	)
+	return `${capitalize(dateAbbreviationMonth)}. ${dateYear}`
+}
+
+export const works = work.map((experience) => ({
+	...experience,
+	startDate: normaliceDate(experience.startDate),
+	endDate:
+		experience.endDate != null ? normaliceDate(experience.endDate) : null,
+}))
